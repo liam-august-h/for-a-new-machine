6a6b0e0e4ed22bb978824b9c7a27f414e258aa18
liam-august-h <liam.august.h@gmail.com>
2016-09-30 12:56:27 +0800

    add pip

--- linux/install.sh
+++ linux/install.sh
@@ -7,12 +7,14 @@ cd ~
 # necessary packages:
 # ack-grep, vim, cscope, ctags, colordiff, tmux, autojump zeal ibus-pinyin openjdk-8-jdk
 sudo apt install ack-grep vim cscope exuberant-ctags  colordiff tmux\
-     autojump zeal ibus-pinyin gawk
+     autojump zeal ibus-pinyin gawk python-pip
 
 sudo apt-get install git-core gnupg flex bison gperf build-essential \
      zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 \
      lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache \
      libgl1-mesa-dev libxml2-utils xsltproc unzip
+
+sudo pip install colout
 
 # FIXME android-ndk, android-sdk, 
 
