c8a83975c8b89b2dc3ba5d3c0533fa503ad486b4
liam-august-h <liam.august.h@gmail.com>
2016-10-10 09:47:37 +0800

    typora, apktool, and open download page with chrome-browser

--- linux/install.sh
+++ linux/install.sh
@@ -16,8 +16,37 @@ sudo apt-get install git-core gnupg flex bison gperf build-essential \
 
 sudo pip install colout
 
-# FIXME android-ndk, android-sdk, 
+# for typora
+# optional, but recommended
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BA300B7755AFCFAE
+# add Typora's repository
+sudo add-apt-repository 'deb https://typora.io ./linux/'
+sudo apt-get update
+# install typora
+sudo apt-get install typora
+
+# apktool
+cd ~/bin
+mkdir apktool-2; cd apktool-2
+wget https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/linux/apktool
+chmod 711 apktool
+wget https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.2.0.jar
+ln -s apktool_2.2.0.jar apktool.jar
+cd ..
+ln -s ./apktool-2/apktool apktool
+cd ~
+
+# backsmali
+
+
+# download something manually. There are:
+# simplenote, android-sdk, android-ndk
+google-chrome https://simplenote.com/downloads/ https://developer.android.com/studio/index.html https://developer.android.com/ndk/downloads/index.html
 
 # disable nautilus automount and automount-open
 #gsettings set org.gnome.desktop.media-handling automount false
 gsettings set org.gnome.desktop.media-handling automount-open false
+
+# FIXME: android SDK NDK
+mkdir -P ~/android/sdk && mkdir -P ~/android/ndk
+#ln -s ~/android/sdk/
