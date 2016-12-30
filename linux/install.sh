01f219a07c2cb70dbd7cd87027d2ba1d2efc092c
liam <liam.august.h@gmail.com>
2016-12-30 09:23:38 +0800

    add notify script and so on.

--- linux/install.sh
+++ linux/install.sh
@@ -6,15 +6,19 @@ cd ~
 
 # necessary packages:
 # ack-grep, vim, cscope, ctags, colordiff, tmux, autojump zeal ibus-pinyin openjdk-8-jdk
-sudo apt install ack-grep vim cscope exuberant-ctags  colordiff tmux\
-     autojump zeal ibus-pinyin gawk python-pip
+sudo apt-get install install ack-grep vim cscope exuberant-ctags  colordiff tmux\
+     autojump gawk zip curl unzip
 
-sudo apt-get install git-core gnupg flex bison gperf build-essential \
-     zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 \
-     lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache \
-     libgl1-mesa-dev libxml2-utils xsltproc unzip
+sudo apt-get install
 
-sudo pip install colout
+# sudo apt-get install zeal ibus-pinyin python-pip
+
+#sudo apt-get install git-core gnupg flex bison gperf build-essential \
+#     zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 \
+#     lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache \
+#     libgl1-mesa-dev libxml2-utils xsltproc
+
+#sudo pip install colout
 
 # for typora
 # optional, but recommended
---
+++ linux/notify.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+notify-send --expire-time=10000 "$@ has already been done"
