054aa1cf541291d4de4bfbf86b49b4cc77307ec6
liam <liam.august.h@gmail.com>
2017-12-09 16:25:37 +0800

    modify something

--- mac/setup_tensorflow.sh
+++ mac/setup_tensorflow.sh
@@ -5,8 +5,8 @@
 brew install python3
 
 # install pip
-easy_install pip3
-pip install --upgrade virtualenv
+#easy_install pip3
+pip3 install --upgrade virtualenv
 
 # create tensorflow 
 virtualenv --system-site-packages ~/tensorflow
@@ -14,8 +14,4 @@ virtualenv --system-site-packages ~/tensorflow
 # active
 source ~/tensorflow/bin/activate 
 
-pip install https://storage.googleapis.com/tensorflow/mac/tensorflow-0.5.0-py2-none-any.whl
-pip install numpy
-pip install scikit-learn
-pip install pandas
-pip install matplotlib
+pip3 install --upgrade tensorflow numpy scikit-learn pandas matplotlib
