2617bf1ea9d87b96d81998100176efc71d1ff7bb
liam <liam.august.h@gmail.com>
2018-12-04 12:20:30 +0800

    rewrite

--- linux/install.sh
+++ linux/install.sh
@@ -1,59 +1,93 @@
 #!/bin/bash
 
-cd ~
-mkdir bin
-cd ~
+set -x
 
-# necessary packages:
-# ack-grep, vim, cscope, ctags, colordiff, tmux, autojump zeal ibus-pinyin openjdk-8-jdk
-sudo apt-get install -y ack-grep vim cscope exuberant-ctags colordiff tmux\
-     autojump gawk zip curl unzip doxygen graphviz doxygen-gui openjdk-8-jdk
+USER=$(whoami)
+if ! [[ -e /home/${USER}/bin ]]; then
+    mkdir /home/${USER}/bin
+fi
 
+# TODO: ping network
+
+sudo apt update -qq
+
+# enable ssh server
+if ! [[ -n $(sudo systemctl status ssh) ]]; then
+    sudo apt install -y openssh-server
+fi
+
+# install all necessary packages:
+## java8
+if ! [[ -n $(which javac) ]]; then
+    echo "install java"
+    sudo apt install -y openjdk-8-jdk openjdk-8-dbg openjdk-8-source
+fi
+
+## TODO: python and python3
+if ! [[ -n $(which python) ]]; then
+    echo "install python"
+    sudo apt install -y -qq python-dev
+fi
+
+if ! [[ -n $(which python3) ]]; then
+    echo "install python3"
+    sudo apt install -y -qq python3-dev
+fi
+
+### always install distutils
+sudo apt -y -qq python3-distutils
+
+PYTHON_VERSION=$(python -c "import sys; print('{:d}'.format(sys.hexversion<0x03000000))")
+if [[ ${PYTHON_VERSION} -eq 0 ]]; then
+    echo 'change python 3 to python 2'
+    if [[ -n $(update-alternatives --list python 2>&1 | grep 'no alternatives for python') ]]; then
+        echo 'install python'
+        sudo update-alternatives --install /usr/bin/python python $(readlink -f /usr/bin/python2) 1
+        sudo update-alternatives --install /usr/bin/python python $(readlink -f /usr/bin/python3) 10
+    fi
+else
+    echo 'keep python 2'
+fi
+
+## toys
+sudo apt install -qq -y ack-grep vim tmux autojump zip unzip
 echo "source /usr/share/autojump/autojump.sh" >> ~/.bashrc
 
-# python related
-sudo apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
+## programming tools
+sudo apt install -qq -y cscope exuberant-ctags curl git
 
-# sudo apt-get install zeal ibus-pinyin python-pip
+## doxygen
+sudo apt install -qq -y doxygen doxygen-gui
 
-#sudo apt-get install git-core gnupg flex bison gperf build-essential \
-#     zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 \
-#     lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache \
-#     libgl1-mesa-dev libxml2-utils xsltproc
+## graphviz
+sudo apt install -qq -y graphviz doxygen-gui
 
-#sudo pip install colout
+### pip
+#if ! [[ -n $(which pip) ]]; then
+#    curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
+#    python /tmp/get-pip.py --user
+#    pip install --user -U pip
+#fi
 
-# for typora
-# optional, but recommended
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BA300B7755AFCFAE
-# add Typora's repository
-sudo add-apt-repository 'deb https://typora.io ./linux/'
-sudo apt-get update
-# install typora
-sudo apt-get install typora
+## pipenv
+if ! [[ -n $(which pipenv) ]]; then
+    curl https://raw.githubusercontent.com/kennethreitz/pipenv/master/get-pipenv.py | sudo python
+fi
 
-# apktool
-cd ~/bin
-mkdir apktool-2; cd apktool-2
-wget https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/linux/apktool
-chmod 711 apktool
-wget https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.2.0.jar
-ln -s apktool_2.2.0.jar apktool.jar
-cd ..
-ln -s ./apktool-2/apktool apktool
-cd ~
+## chrome
+if ! [[ -n $(which google-chrome) ]]; then
+    echo "to download chrome from https://www.google.com/chrome/ "
+    wget -P /tmp https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb
+    echo "google-chrome needs environment variables, like all_proxy, no_proxy"
+fi
 
-# backsmali
+## synergy
+if ! [[ -n $(which synergy) ]]; then
+    echo "to download synergy from https://symless.com/synergy with gmail and H1 as account "
+fi
 
+# clean
+sudo apt autoremove -y -qq
 
-# download something manually. There are:
-# simplenote, android-sdk, android-ndk
-google-chrome https://simplenote.com/downloads/ https://developer.android.com/studio/index.html https://developer.android.com/ndk/downloads/index.html
-
-# disable nautilus automount and automount-open
-#gsettings set org.gnome.desktop.media-handling automount false
-gsettings set org.gnome.desktop.media-handling automount-open false
-
-# FIXME: android SDK NDK
-mkdir -P ~/android/sdk && mkdir -P ~/android/ndk
-#ln -s ~/android/sdk/
+set +x
